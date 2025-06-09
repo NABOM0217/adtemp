07be75520f61e32a51a2e39babc9b140f6a218de
NABOM0217 <nabom0217@gmail.com>
2025-06-09 15:47:25 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'slide-in-fwd-bottom': 'slide-in-fwd-bottom 0.6s ease-out both',
+      },
+      keyframes: {
+        'slide-in-fwd-bottom': {
+          '0%': {
+            transform: 'translateZ(-1400px) translateY(80px)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateZ(0) translateY(0)',
+            opacity: '1',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
