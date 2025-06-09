a9924d12939d20f62811f20d77a8e55d2442324c
NABOM0217 <nabom0217@gmail.com>
2025-06-09 13:02:06 +0900

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,8 +18,11 @@ export default function Hero() {
       <p
         className="text-lg md:text-2xl mb-6 text-white slide-in-fwd-bottom"
         style={{ animationDelay: '0.7s' }}
-        {/*style={{ textShadow: '0 1px 2px rgba(0, 0, 0, 0.4)' }}*/} //원래 텍스트 스타일
-      >
+              
+       /* 원래 텍스트 스타일 : 
+       style={{ textShadow: '0 1px 2px rgba(0, 0, 0, 0.4)' }} //원래 텍스트 스타일
+      */
+       >
         광고의 미세한 온도차가, 결과의 큰 차이를 만듭니다. 
         <br />
         섬세하게 뜨겁고, 따뜻하게 정직한 광고.
