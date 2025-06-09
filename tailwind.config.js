331fc277e1246f5700822f365ac29107db07e056
NABOM0217 <nabom0217@gmail.com>
2025-06-09 15:18:03 +0900

    repair2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
-extend: {
-  animation: {
-    'slide-in-fwd-bottom': 'slide-in-fwd-bottom 0.6s ease-out both',
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
-  keyframes: {
-    'slide-in-fwd-bottom': {
-      '0%': {
-        transform: 'translateZ(-1400px) translateY(80px)',
-        opacity: '0',
-      },
-      '100%': {
-        transform: 'translateZ(0) translateY(0)',
-        opacity: '1',
-      },
-    },
-  },
-},
+  plugins: [],
+};
