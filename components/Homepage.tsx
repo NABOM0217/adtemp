eac67152dde03ee809893f719e256fcd415addf9
NABOM0217 <nabom0217@gmail.com>
2025-06-09 13:27:58 +0900

    Update Homepage.tsx

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,6 +1,13 @@
+
+import { useScrollAnimation } from '../hooks/useScrollAnimation';
+
+
 export default function Homepage() {
+   const ref = useScrollAnimation();
   return (
-    <section id="Homepage" className="px-6 py-28 bg-white text-center">
+    <section 
+      ref={ref}
+      id="Homepage" className="px-6 py-28 bg-white text-center invisible-before">
       <h2 className="text-3xl font-bold mb-4">홈페이지 제작 및 관리</h2>
       <p className="mb-8 text-gray-600">홈페이지 기획부터 제작, 관리, 업데이트 원스톱 서비스</p>
       
